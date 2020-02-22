6778be95178292a38bf7cc60e2f1c3b8838b0db3
NovaSoft <61245134+novasoftkg@users.noreply.github.com>
2020-02-22 21:37:05 +0600

    Add files via upload

--- js/core.js
+++ js/core.js
@@ -1,1 +1,41 @@
+$(document).ready(function() {
+    $('.works-slide').slick({
+        dots: true,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        asNavFor: '.works-slide-phone'
+    });
+    $('.works-slide-phone').slick({
+        dots: false,
+        arrows: false
+    });
 
+    $('#main-nav-btn').click(function(){
+        $('.navigation_block').slideToggle(700);
+        if($(this).hasClass('not-active')) {
+            $(this).addClass('active').removeClass('not-active');
+        }else{
+            setTimeout(function(){
+                $('.active').addClass('not-active').removeClass('active');
+            },600)
+        }
+    });
+
+    $(window).bind("resize load", checkPosition);
+
+    function checkPosition()
+    {
+        if($(window).innerWidth() > 948)
+        {
+            $('.navigation_block').css('display','block');
+        }
+        else{
+            $('.navigation_block').css('display','none');
+            setTimeout(function(){
+                $('.active').addClass('not-active').removeClass('active');
+            },600);
+        }
+    }
+
+});
